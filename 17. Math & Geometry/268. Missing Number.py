d95e15f36fbe0c57b696efebe34d85504052d97c
prajesh22 <prajesh22@gmail.com>
2023-08-16 14:55:53 -0400

    Missing number

---
+++ 17. Math & Geometry/268. Missing Number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def missingNumber(self, nums: list[int]) -> int:
+        length = len(nums)
+        return (length * (length + 1)) // 2 - sum(nums)
+
+
+exampleInputs = [
+    [3, 0, 1],
+    [0, 1],
+    [9, 6, 4, 2, 3, 5, 7, 0, 1],
+]
+for n in exampleInputs:
+    print(Solution().missingNumber(n))
