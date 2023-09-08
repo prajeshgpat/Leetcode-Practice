37670f2f4b01637ad8bfe47b34d5c17b74bd8ead
prajesh22 <prajesh22@gmail.com>
2023-09-08 00:06:21 -0400

    pascal's triangle

---
+++ 13. 1-D Dynamic Programming/118. Pascal's Triangle.py
@@ -0,0 +1,22 @@
+class Solution:
+    def generate(self, numRows: int) -> list[list[int]]:
+        pascal = [[1]]
+        for row in range(1, numRows):
+            prevRow = pascal[-1]
+            currRow = [1]
+            for i in range(1, row):
+                currRow.append(prevRow[i - 1] + prevRow[i])
+
+            currRow.append(1)
+            pascal.append(currRow)
+
+        return pascal
+
+
+testInputs = [
+    5,
+    1,
+    10,
+]
+for numRows in testInputs:
+    print(Solution().generate(numRows))
